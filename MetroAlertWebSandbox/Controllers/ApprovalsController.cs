4ac5d0ecda56fe9ed55b5d237176f3443e23e267
psubianto <psubianto@users.noreply.github.com>
2018-04-11 06:13:14 -0400

    Add files via upload

---
+++ MetroAlertWebSandbox/Controllers/ApprovalsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetroAlertWebSandbox.Controllers
+{
+    public class ApprovalsController : Controller
+    {
+        // GET: Approvals
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
